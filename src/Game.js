9bb417539b30366f2674a1a661357d3931a47825
Julia Thomas <juliathomas@outlook.com>
2022-06-23 16:38:51 +0100

    added functions to Game.js

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,43 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Game extends React.Component {
+
+  constructor() {
+    super();
+    this.rows = HEIGHT/CELL_SIZE;
+    this.cols = WIDTH/CELL_SIZE;
+    this.board = this.makeEmptyBoard();
+  }
+
+  state = { cells: [], }
+
+  // CREATE EMPTY BOARD
+  makeEmptyBoard() {
+    let board = [];
+
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  }
+
+  // CREATE CELLS FROM this.board
+  makeCells() {
+    let cells = [];
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({x, y});
+        }
+      }
+    }
+    return cells;
+  }
+
   render () {
     return (
       <div>
